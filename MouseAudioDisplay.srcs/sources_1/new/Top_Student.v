ace655c0a8d4cf6c2636287705b2d56b9843350d
ong-ck <e0774208@u.nus.edu>
2023-02-27 11:55:42 +0800

    Display testing

--- MouseAudioDisplay.srcs/sources_1/new/Top_Student.v
+++ MouseAudioDisplay.srcs/sources_1/new/Top_Student.v
@@ -13,8 +13,37 @@
 
 module Top_Student (
     // Delete this comment and include Basys3 inputs and outputs here
+    input clock,
+    input sw4,
+    output [7:0] JC
     );
 
     // Delete this comment and write your codes and instantiations here
-
+    
+    reg [15:0] oled_data = (sw4) ? 16'hF800 : 16'h07E0;
+    wire frame_begin, sending_pixels, sample_pixel;
+    wire [12:0] pixel_index;
+    
+    wire clk6p25m;
+    clk_divider my_clk6p25m (.basys_clk(clock), .m(2499), .new_clk(clk6p25m));
+    
+    Oled_Display oled_unit_one(
+        .clk(clk6p25m), 
+        .reset(0), 
+        .frame_begin(frame_begin), 
+        .sending_pixels(sending_pixels), 
+        .sample_pixel(sample_pixel), 
+        .pixel_index(pixel_index), 
+        .pixel_data(oled_data), 
+        .cs(JC[0]), 
+        .sdin(JC[1]), 
+        .sclk(JC[3]), 
+        .d_cn(JC[4]), 
+        .resn(JC[5]), 
+        .vccen(JC[6]),
+        .pmoden(JC[7])
+    );
+    
+    
+    
 endmodule
