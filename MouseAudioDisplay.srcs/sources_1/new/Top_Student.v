c21b910fb4299f1e8a2e722ae7d36985824d0792
heejet <heejet@gmail.com>
2023-03-02 23:23:58 +0800

    Add comments

--- MouseAudioDisplay.srcs/sources_1/new/Top_Student.v
+++ MouseAudioDisplay.srcs/sources_1/new/Top_Student.v
@@ -20,12 +20,15 @@ module Top_Student (
     output [7:0] JC,
     output reg [15:13] led
     );
+    // Mouse Test 
     wire left, middle, right;
-    MouseCtl mouse_test(.clk(basys_clock), .rst(btn_C), .value(0), .setx(0), 
-                        .sety(0), .setmax_x(0), .setmax_y(0),
-                        .left(left), .middle(middle), .right(right),
-                        .ps2_clk(ps2_clk), .ps2_data(ps2_data)
-                        );
+
+    MouseCtl mouse_test(
+        .clk(basys_clock), .rst(btn_C), .value(0), .setx(0), 
+        .sety(0), .setmax_x(0), .setmax_y(0),
+        .left(left), .middle(middle), .right(right),
+        .ps2_clk(ps2_clk), .ps2_data(ps2_data)
+    );
     
     always @ (posedge basys_clock) begin
         led[15] <= left;
@@ -33,12 +36,14 @@ module Top_Student (
         led[13] <= right;
     end
 
+    // Display Test
+
     reg [15:0] oled_data = (sw4) ? 16'hF800 : 16'h07E0;
     wire frame_begin, sending_pixels, sample_pixel;
     wire [12:0] pixel_index;
     
     wire clk6p25m;
-    clk_divider my_clk6p25m (.basys_clk(basys_clock), .m(2499), .new_clk(clk6p25m));
+    clk_divider my_clk6p25m(.basys_clk(basys_clock), .m(2499), .new_clk(clk6p25m));
     
     Oled_Display oled_unit_one(
         .clk(clk6p25m), 
