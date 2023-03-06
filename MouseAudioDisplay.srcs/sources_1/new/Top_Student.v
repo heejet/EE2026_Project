95d115b5f920cbec84fb67b141153f206a89774a
nikkiDEEE <e0725992@u.nus.edu>
2023-03-06 09:15:38 +0800

    Add level 1 input audio task

--- MouseAudioDisplay.srcs/sources_1/new/Top_Student.v
+++ MouseAudioDisplay.srcs/sources_1/new/Top_Student.v
@@ -12,9 +12,24 @@
 
 
 module Top_Student (
-    // Delete this comment and include Basys3 inputs and outputs here
+    input clock,
+    input J_MIC_Pin3,
+    input SW1,
+    output J_MIC_Pin1,
+    output J_MIC_Pin4,
+    output [11:0] led
     );
+    
+    wire clk10k;
+    wire clk20k;
+    wire main_clk;
+    wire [11:0] MIC_in;
+    
+    clk_divider my_10khz_clock(.basys_clk(clock),.m(4999999),.new_clk(clk10k));
+    clk_divider my_20khz_clock(.basys_clk(clock),.m(2499),.new_clk(clk20k));
+    Audio_Input AI_dut(.CLK(clock),.cs(main_clk),.MISO(J_MIC_Pin3),.clk_samp(J_MIC_Pin1),.sclk(J_MIC_Pin4),.sample(MIC_in));
 
-    // Delete this comment and write your codes and instantiations here
+    assign main_clk = (SW1 == 1) ? clk10k : clk20k;
+    assign led = MIC_in;
 
 endmodule
